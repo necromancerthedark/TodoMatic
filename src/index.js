0b4514b8b180e60dddeed145ed2b0cec147dbf6d
necromancerthedark <kg22584@gmail.com>
2022-02-02 12:52:11 +0530

    working! almost all features!

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const DATA = [
-  {name :"Eat" ,completed : true, id : "todo-0"},
-  {name : "Sleep" ,completed :false ,id : "todo-1"},
-  {name : "Repeat" ,completed :false, id : "todo-2"}
+  { name: "Eat", completed: true, id: "todo-0" },
+  { name: "Sleep", completed: false, id: "todo-1" },
+  { name: "Repeat", completed: false, id: "todo-2" },
 ];
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    ReactDOM
+    <App taskData={DATA} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-ReactDOM.render(
-<App subject= "Kumar"/>, 
-document.getElementById('root')
-);
-
-ReactDOM.render(<App tasks= {DATA}/> , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
